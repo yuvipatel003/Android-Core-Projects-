385bc073e01e691914b86b52a8d23af2deffb71e
Yuvraj Patel <yuvipatel003@gmail.com>
2020-03-26 20:34:15 -0400

    Added button and click.

--- Notification/Notification/app/src/main/java/com/yuvrajpatel/notification/MainActivity.java
+++ Notification/Notification/app/src/main/java/com/yuvrajpatel/notification/MainActivity.java
@@ -1,14 +1,30 @@
 package com.yuvrajpatel.notification;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnCreateNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCreateNotification = findViewById(R.id.notify);
+        mBtnCreateNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+            }
+        });
     }
+
+    public void createNotification() {}
 }
+

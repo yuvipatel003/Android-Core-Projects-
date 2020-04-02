6c2b9ddbb0b413f54785d7e10c88b73ada9e80da
Yuvraj Patel <yuvipatel003@gmail.com>
2020-04-01 21:50:25 -0400

    Create notification on button click.

--- JobScheduler/JobScheduler/app/src/main/java/com/yuvrajpatel/jobscheduler/MainActivity.java
+++ JobScheduler/JobScheduler/app/src/main/java/com/yuvrajpatel/jobscheduler/MainActivity.java
@@ -2,13 +2,90 @@ package com.yuvrajpatel.jobscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mBtnScheduleJob, mBtnCancelJob;
+    private JobScheduler mScheduler;
+    private static final int JOB_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnScheduleJob = findViewById(R.id.btn_schedule_job);
+        mBtnCancelJob = findViewById(R.id.btn_cancel_job);
+
+        mBtnScheduleJob.setOnClickListener(this);
+        mBtnCancelJob.setOnClickListener(this);
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
     }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.btn_schedule_job:
+                scheduleJob();
+                break;
+
+            case R.id.btn_cancel_job:
+                cancelJob();
+                break;
+
+                default:
+        }
+
+    }
+
+    public void scheduleJob() {
+        RadioGroup networkOptions = findViewById(R.id.radiogroup_network);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        switch(selectedNetworkID){
+            case R.id.radio_network_none:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.radio_network_any:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.radio_network_wifi:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder.setRequiredNetworkType(selectedNetworkOption);
+
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraints are met.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelJob(){
+        if (mScheduler!=null){
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }

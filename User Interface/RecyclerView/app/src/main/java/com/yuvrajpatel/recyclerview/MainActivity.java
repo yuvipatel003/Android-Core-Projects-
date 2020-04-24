782c23f3c7b849ef4c6b1553fceb8a097226973d
Yuvraj Patel <yuvipatel003@gmail.com>
2020-04-23 21:55:56 -0400

    Update MainActivity.java

--- User Interface/RecyclerView/app/src/main/java/com/yuvrajpatel/recyclerview/MainActivity.java
+++ User Interface/RecyclerView/app/src/main/java/com/yuvrajpatel/recyclerview/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Put initial data into the word list.
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
     }
 }

216cab78f40f372335cf52fced00752164a52ea7
Yuvraj Patel <yuvipatel003@gmail.com>
2020-04-26 21:25:51 -0400

    Added adapter class.

---
+++ User Interface/RecyclerView/app/src/main/java/com/yuvrajpatel/recyclerview/WordListAdapter.java
@@ -0,0 +1,39 @@
+package com.yuvrajpatel.recyclerview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>
+{
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+        
+        public WordViewHolder(@NonNull View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+    }
+}

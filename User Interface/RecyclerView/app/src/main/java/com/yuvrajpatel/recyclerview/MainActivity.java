6011acdac83bbc419b65728dec104b15691b9a72
Yuvraj Patel <yuvipatel003@gmail.com>
2020-04-27 22:53:49 -0400

    Added recyclerview.

--- User Interface/RecyclerView/app/src/main/java/com/yuvrajpatel/recyclerview/MainActivity.java
+++ User Interface/RecyclerView/app/src/main/java/com/yuvrajpatel/recyclerview/MainActivity.java
@@ -1,14 +1,19 @@
 package com.yuvrajpatel.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Log.d("Size:",String.valueOf(mAdapter.getItemCount()));
     }
 }

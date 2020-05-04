29cbde75b7f7c22add1fd72e6d1d8810adeb5a81
Yuvraj Patel <yuvipatel003@gmail.com>
2020-05-03 21:01:47 -0400

    Open dial pad for phone number field.

--- User Interface/InputControl/app/src/main/java/com/yuvrajpatel/inputcontrol/MainActivity.java
+++ User Interface/InputControl/app/src/main/java/com/yuvrajpatel/inputcontrol/MainActivity.java
@@ -2,13 +2,60 @@ package com.yuvrajpatel.inputcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        if (editTextPhoneNumber != null) {
+            editTextPhoneNumber.setOnEditorActionListener
+                    (new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                            boolean handled = false;
+                            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                                dialNumber();
+                                handled = true;
+                            }
+                            return handled;
+                        }
+                    });
+        }
+    }
+
+    private void dialNumber() {
+        // Find the editTextPhoneNumber view.
+        EditText editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        String phoneNum = null;
+        // If the editTextPhoneNumber field is not null,
+        // concatenate "tel: " with the phone number string.
+        if (editTextPhoneNumber != null) phoneNum = "tel:" +
+                editTextPhoneNumber.getText().toString();
+        // Optional: Log the concatenated phone number for dialing.
+        Log.d(TAG, "dialNumber: " + phoneNum);
+        // Specify the intent.
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        intent.setData(Uri.parse(phoneNum));
+        // If the intent resolves to a package (app),
+        // start the activity with the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
     }
 }

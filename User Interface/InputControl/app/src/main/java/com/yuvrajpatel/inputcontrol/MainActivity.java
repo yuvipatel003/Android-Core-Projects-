19d399cf44df51fef46e346b0792ddc53f02465d
Yuvraj Patel <yuvipatel003@gmail.com>
2020-05-05 21:32:35 -0400

    added java doc.

--- User Interface/InputControl/app/src/main/java/com/yuvrajpatel/inputcontrol/MainActivity.java
+++ User Interface/InputControl/app/src/main/java/com/yuvrajpatel/inputcontrol/MainActivity.java
@@ -36,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Opening external app
+     */
     private void dialNumber() {
         // Find the editTextPhoneNumber view.
         EditText editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
